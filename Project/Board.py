d7437700ae06f42dfa321445e947eaf2edd83867
tmam101 <thomaspgoss98@gmail.com>
2019-10-27 12:24:03 -0400

    Add Board empty cells

---
+++ Project/Board.py
@@ -0,0 +1,8 @@
+class Board:
+    cells = [[[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]],
+        [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]],
+        [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]],
+        [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]]]
+
+# TODO ToString
+# Model free active q learning?
--- Project/Main.py
+++ Project/Main.py
@@ -1,1 +1,4 @@
+from Board import Board
+board = Board()
 print("hey")
+print(board.cells)
