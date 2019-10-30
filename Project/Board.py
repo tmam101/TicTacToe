8a1c374a6358b3e821a5eeae189b525a8677883f
Ellie Hadden-Ford <elliehf1997@gmail.com>
2019-10-30 09:24:05 -0400

    some TODOs written

--- Project/Board.py
+++ Project/Board.py
@@ -6,3 +6,15 @@ class Board:
 
 # TODO ToString
 # Model free active q learning?
+
+
+
+# TODO make win/lose/draw/incomplete function
+
+
+# TODO make reward function
+# reward with value of 1 if putting a move in that square leads to a win?
+# average the value each iteration(trial)? is this the same as value iteration?
+
+# TODO make choose move function
+# 
--- Project/Main.py
+++ Project/Main.py
@@ -2,3 +2,8 @@ from Board import Board
 board = Board()
 print("hey")
 print(board.cells)
+
+
+# need to get input that tells how many trials to run-three numbers
+# maybe make a train function that we can call with number of trials?
+# and the function can print out values once it finishes
