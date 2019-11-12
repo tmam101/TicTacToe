6de5a1bec7baef3ee6498cf2a2b06a4bf4211c84
Tony Bird <tonybird@live.unc.edu>
2019-11-12 09:53:03 -0500

    Finished win checking

--- Project/Board.py
+++ Project/Board.py
@@ -37,37 +37,68 @@ class Board:
 # TODO make win/lose/draw/incomplete function
     def win(self):
         cells = self.cells
-        for a in range(4):
-            for b in range(4):
-                # Check horizontals and verticals
-                if "X" == cells[a][b][0] == cells[a][b][1] == cells[a][b][2] == cells[a][b][3]: return 1
-                if "O" == cells[a][b][0] == cells[a][b][1] == cells[a][b][2] == cells[a][b][3]: return -1
-                if "X" == cells[a][0][b] == cells[a][1][b] == cells[a][2][b] == cells[a][3][b]: return 1
-                if "O" == cells[a][0][b] == cells[a][1][b] == cells[a][2][b] == cells[a][3][b]: return -1
-                if "X" == cells[0][a][b] == cells[1][a][b] == cells[2][a][b] == cells[3][a][b]: return 1
-                if "O" == cells[0][a][b] == cells[1][a][b] == cells[2][a][b] == cells[3][a][b]: return -1
 
+        # On the 4x4x4 board, there are 76 winning lines. On each of the four 4x4 boards, or horizontal planes,
+        # there are four columns, four rows, and two diagonals, accounting for 40 lines. There are 16 vertical
+        # lines, each ascending from a cell on the bottom board through the corresponding cells on the other
+        # boards. There are eight vertically-oriented planes parallel to the sides of the boards, each of
+        # these adding two more diagonals (the horizontal and vertical lines of these planes have already
+        # been counted). Finally, there are two vertically-oriented planes that include the diagonal lines of
+        # the 4x4 boards, and each of these contributes two more diagonal lines- each of these including two
+        # corners and two internal cells.                        https://en.wikipedia.org/wiki/3D_tic-tac-toe
 
-        # for a in range(4):
-        #         # Check diagonals
-        #         if "X" == cells[0][a][a] == cells[1][a][a] == cells[0][a][2] == cells[a][b][3]: return 1
-        #         if "O" == cells[a][b][0] == cells[a][b][1] == cells[a][b][2] == cells[a][b][3]: return -1
-        #         if "X" == cells[a][0][b] == cells[a][1][b] == cells[a][2][b] == cells[a][3][b]: return 1
-        #         if "O" == cells[a][0][b] == cells[a][1][b] == cells[a][2][b] == cells[a][3][b]: return -1
-        #         if "X" == cells[0][a][b] == cells[1][a][b] == cells[2][a][b] == cells[3][a][b]: return 1
-        #         if "O" == cells[0][a][b] == cells[1][a][b] == cells[2][a][b] == cells[3][a][b]: return -1
+        # 4 horizontal planes, 10 wins each:
+        for z in range(4):
+            # 4 rows
+            for x in range(4):
+                if "X" == cells[x][0][z] == cells[x][1][z] == cells[x][2][z] == cells [x][3][z]: return 1
+                if "O" == cells[x][0][z] == cells[x][1][z] == cells[x][2][z] == cells [x][3][z]: return -1
 
-        # Different planes
+            # 4 columns
+            for y in range(4):
+                if "X" == cells[0][y][z] == cells[1][y][z] == cells[2][y][z] == cells[3][y][z]: return 1
+                if "O" == cells[0][y][z] == cells[1][y][z] == cells[2][y][z] == cells[3][y][z]: return -1
+
+            # 2 diagonals
+            if "X" == cells[0][0][z] == cells[1][1][z] == cells[2][2][z] == cells[3][3][z]: return 1
+            if "O" == cells[0][0][z] == cells[1][1][z] == cells[2][2][z] == cells[3][3][z]: return -1
+            if "X" == cells[3][0][z] == cells[2][1][z] == cells[1][2][z] == cells[0][3][z]: return 1
+            if "O" == cells[3][0][z] == cells[2][1][z] == cells[1][2][z] == cells[0][3][z]: return -1
+
+        # 16 vertically oriented lines
+        for x in range(4):
+            for y in range(4):
+                if "X" == cells[x][y][0] == cells[x][y][1] == cells[x][y][2] == cells[x][y][3]: return 1
+                if "O" == cells[x][y][0] == cells[x][y][1] == cells[x][y][2] == cells[x][y][3]: return -1
+
+        # 4 vertical planes parallel to x, 2 diagonals each
+        for y in range(4):
+            if "X" == cells[0][y][0] == cells[1][y][1] == cells[2][y][2] == cells[3][y][3]: return 1
+            if "O" == cells[0][y][0] == cells[1][y][1] == cells[2][y][2] == cells[3][y][3]: return -1
+            if "X" == cells[3][y][0] == cells[2][y][1] == cells[1][y][2] == cells[0][y][3]: return 1
+            if "O" == cells[3][y][0] == cells[2][y][1] == cells[1][y][2] == cells[0][y][3]: return -1
+
+        # 4 vertical planes parallel to y, 2 diagonals each
+        for x in range(4):
+            if "X" == cells[x][0][0] == cells[x][1][1] == cells[x][2][2] == cells[x][3][3]: return 1
+            if "O" == cells[x][0][0] == cells[x][1][1] == cells[x][2][2] == cells[x][3][3]: return -1
+            if "X" == cells[x][3][0] == cells[x][2][1] == cells[x][1][2] == cells[x][0][3]: return 1
+            if "O" == cells[x][3][0] == cells[x][2][1] == cells[x][1][2] == cells[x][0][3]: return -1
+
+        # 4 long diagonals
         if "X" == cells[0][0][0] == cells[1][1][1] == cells[2][2][2] == cells[3][3][3]: return 1
-        if "X" == cells[0][3][0] == cells[1][2][1] == cells[2][1][2] == cells[3][0][3]: return 1
-        if "X" == cells[0][0][3] == cells[1][1][2] == cells[2][2][1] == cells[3][3][0]: return 1
-        if "X" == cells[0][3][3] == cells[1][2][2] == cells[2][1][1] == cells[3][0][0]: return 1
         if "O" == cells[0][0][0] == cells[1][1][1] == cells[2][2][2] == cells[3][3][3]: return -1
+
+        if "X" == cells[0][3][0] == cells[1][2][1] == cells[2][1][2] == cells[3][0][3]: return 1
         if "O" == cells[0][3][0] == cells[1][2][1] == cells[2][1][2] == cells[3][0][3]: return -1
-        if "O" == cells[0][0][3] == cells[1][1][2] == cells[2][2][1] == cells[3][3][0]: return -1
-        if "O" == cells[0][3][3] == cells[1][2][2] == cells[2][1][1] == cells[3][0][0]: return -1
 
+        if "X" == cells[3][0][0] == cells[2][1][1] == cells[1][2][2] == cells[0][3][3]: return 1
+        if "O" == cells[3][0][0] == cells[2][1][1] == cells[1][2][2] == cells[0][3][3]: return -1
 
+        if "X" == cells[3][3][0] == cells[2][2][1] == cells[1][1][2] == cells[0][0][3]: return 1
+        if "O" == cells[3][3][0] == cells[2][2][1] == cells[1][1][2] == cells[0][0][3]: return -1
+
+        return 0;
 
         # return 0 if not enough info/game still in progress
         # return -1 if draw?
